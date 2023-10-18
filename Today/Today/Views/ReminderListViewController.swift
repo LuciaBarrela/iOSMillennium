2a80f07c5eb7f61cba211d57c8e0ea45ebe4764a
LuciaBarrela <lucia.sofiabb@gmail.com>
2023-10-18 11:36:26 +0100

    new restart
    nova app Today

---
+++ Today/Today/Views/ReminderListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Lucia Barrela on 18/10/2023.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+        
+        
+        
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) {
+                    (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(
+                           using: cellRegistration, for: indexPath, item: itemIdentifier)
+                }
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+           var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+           listConfiguration.showsSeparators = false
+           listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+       }
+   }
