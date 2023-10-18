45611df8015cfbf3abc7b9a1b8ad9f0e6d5db538
LuciaBarrela <lucia.sofiabb@gmail.com>
2023-10-18 11:41:57 +0100

    displaying cell info

--- Today/Today/Views/ReminderListViewController.swift
+++ Today/Today/Views/ReminderListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
     
     var dataSource: DataSource!
 
@@ -33,6 +34,17 @@ class ReminderListViewController: UICollectionViewController {
             return collectionView.dequeueConfiguredReusableCell(
                            using: cellRegistration, for: indexPath, item: itemIdentifier)
                 }
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        var reminderTitles = [String]()
+               for reminder in Reminder.sampleData {
+                   reminderTitles.append(reminder.title)
+               }
+               snapshot.appendItems(reminderTitles)
+        dataSource.apply(snapshot)
+        
+        collectionView.dataSource = dataSource
     }
 
     private func listLayout() -> UICollectionViewCompositionalLayout {
