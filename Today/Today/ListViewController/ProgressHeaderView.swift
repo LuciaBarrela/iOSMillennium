f20b47b9a0f7c76fab2d78f0efda4fc38fc9ab75
LuciaBarrela <lucia.sofiabb@gmail.com>
2023-10-23 11:29:58 +0100

    accessibility to progress view

--- Today/Today/ListViewController/ProgressHeaderView.swift
+++ Today/Today/ListViewController/ProgressHeaderView.swift
@@ -13,6 +13,7 @@ class ProgressHeaderView: UICollectionReusableView {
     
     var progress: CGFloat = 0 {
            didSet {
+               setNeedsLayout()
                heightConstraint?.constant = progress * bounds.height
                UIView.animate(withDuration: 0.2) { [weak self] in
                              self?.layoutIfNeeded()
@@ -25,11 +26,20 @@ class ProgressHeaderView: UICollectionReusableView {
     private let lowerView = UIView(frame: .zero)
     private let containerView = UIView(frame: .zero)
     private var heightConstraint: NSLayoutConstraint?
+    private var valueFormat: String {
+          NSLocalizedString("%d percent", comment: "progress percentage value format")
+      }
     
     override init(frame: CGRect) {
             super.init(frame: frame)
         prepareSubviews()
-        }
+        
+        
+        
+        isAccessibilityElement = true
+        accessibilityLabel = NSLocalizedString("Progress", comment: "Progress view accessibility label")
+        accessibilityTraits.update(with: .updatesFrequently)
+    }
     
     required init?(coder: NSCoder) {
            fatalError("init(coder:) has not been implemented")
@@ -37,6 +47,7 @@ class ProgressHeaderView: UICollectionReusableView {
     
     override func layoutSubviews() {
            super.layoutSubviews()
+        accessibilityValue = String(format: valueFormat, Int(progress * 100.0))
            heightConstraint?.constant = progress * bounds.height
            containerView.layer.masksToBounds = true
            containerView.layer.cornerRadius = 0.5 * containerView.bounds.width
