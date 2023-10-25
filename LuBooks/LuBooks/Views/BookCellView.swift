486626fa7dcbf68edff8ec541fcd83121bde8bd7
LuciaBarrela <lucia.sofiabb@gmail.com>
2023-10-25 17:53:12 +0100

    updated bookcellview

--- LuBooks/LuBooks/Views/BookCellView.swift
+++ LuBooks/LuBooks/Views/BookCellView.swift
@@ -5,7 +5,6 @@
 //  Created by Lucia Barrela on 25/10/2023.
 //
 
-
 import SwiftUI
 import SDWebImageSwiftUI
 import CoreData
@@ -14,9 +13,9 @@ struct BookCellView: View {
     @ObservedObject var book: Book
 
     var body: some View {
-        HStack {
-            if !book.imurl.isEmpty {
-                WebImage(url: URL(string: book.imurl))
+        VStack(spacing: 10) {
+            if let thumbnailURL = URL(string: book.imurl) {
+                WebImage(url: thumbnailURL)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 80, height: 120)
@@ -28,15 +27,30 @@ struct BookCellView: View {
                     .frame(width: 80, height: 120)
             }
 
-            VStack(alignment: .leading) {
-                Text(book.title)
-                    .font(.headline)
-                    .lineLimit(2)
+            Text(book.title)
+                .font(.headline)
+                .lineLimit(3)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 10)
 
-                Text(book.authors)
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-            }
+            Text(authorsText(authors: book.authors))
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .lineLimit(1)
+                .padding(.horizontal, 10)
         }
+        .padding(.top, 10) // Add space at the top
+        .padding(10)
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+    
+    private func authorsText(authors: String) -> String {
+        let authorList = authors.split(separator: ",").map { String($0.trimmingCharacters(in: .whitespaces)) }
+        if authorList.count > 1 {
+            return "\(authorList[0]) (...)"
+        }
+        return authorList[0]
     }
 }
