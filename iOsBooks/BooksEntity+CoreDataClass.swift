fcc52cc824f00dcbaaaf5cd9e02f56c65729b73a
LuciaBarrela <lucia.sofiabb@gmail.com>
2023-10-25 17:08:57 +0100

    backup

---
+++ iOsBooks/BooksEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  BooksEntity+CoreDataClass.swift
+//  iOsBooks
+//
+//  Created by Lucia Barrela on 25/10/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BooksEntity)
+public class BooksEntity: NSManagedObject {
+
+}
---
+++ iOsBooks/iOsBooks/iOsBooksApp.swift
@@ -0,0 +1,18 @@
+//
+//  iOsBooksApp.swift
+//  iOsBooks
+//
+//  Created by Lucia Barrela on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOsBooksApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
