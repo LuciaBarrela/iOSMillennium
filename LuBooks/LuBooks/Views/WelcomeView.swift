c485c84d9433766a601e110cc0e71f7957c9c1e5
LuciaBarrela <lucia.sofiabb@gmail.com>
2023-10-27 17:06:51 +0100

    updated 27 Oct
    Message view, Context

--- LuBooks/LuBooks/Views/WelcomeView.swift
+++ LuBooks/LuBooks/Views/WelcomeView.swift
@@ -17,38 +17,42 @@ struct WelcomeView: View {
                 .ignoresSafeArea()
 
             VStack {
+                Spacer() // Pushes content to the vertical center
                 Text("Welcome to LuBooks!")
-                    .font(.largeTitle)
+                    .font(.custom("Pacifico-Regular", size: 35))
                     .foregroundColor(.white)
-                    .padding(.bottom, 20)
-
-                Text("Your Reading Companion")
-                    .font(.title)
+                
+            
+                Text("Your Reading Companion for iOS Development learning")
+                    .font(.subheadline)
                     .foregroundColor(.white)
                     .padding(.bottom, 40)
-
+                
                 JumpingBookIcon()
-                    .frame(width: 100, height: 100)
-                    .padding(.top, 20)
-
-                // Start Reading button
-                Button(action: {
-                    // When the button is pressed, toggle the showContentView state
-                    showContentView.toggle()
-                }) {
-                    Text("Start Reading")
-                        .font(.title)
+                        .frame(width: 100, height: 100)
                         .foregroundColor(.white)
-                        .frame(width: 200, height: 50)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.purple.opacity(0.8)]), startPoint: .leading, endPoint: .trailing))
-                        .cornerRadius(25)
-                }
-                .padding(.top, 20)
+                        .padding(.top, 20)
+                    
+                    Spacer() // Add a spacer to separate the icon and the button
+                    
+                    // Start Reading button
+                    Button(action: {
+                        // When the button is pressed, toggle the showContentView state
+                        showContentView.toggle()
+                    }) {
+                        Text("Start Reading")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 50)
+                            .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.purple.opacity(0.8)]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(25)
+                    }
+                    .padding(.top, 20)
             }
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)) // Add horizontal padding to the VStack
         }
     }
 }
-
 
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
