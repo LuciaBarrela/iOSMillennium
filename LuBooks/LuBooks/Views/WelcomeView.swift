396aba75be6f392f6220425d368c767f1351a498
LuciaBarrela <lucia.sofiabb@gmail.com>
2023-10-27 14:49:33 +0100

    welcomebackgroundcolors

--- LuBooks/LuBooks/Views/WelcomeView.swift
+++ LuBooks/LuBooks/Views/WelcomeView.swift
@@ -12,34 +12,43 @@ struct WelcomeView: View {
     @Binding var showContentView: Bool
 
     var body: some View {
-        VStack {
-            Text("Welcome to LuBooks!")
-                .font(.largeTitle)
-                .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.6), Color.purple.opacity(0.6)]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
 
-            Text("Your Reading Companion")
-                .font(.title)
-
-            JumpingBookIcon()
-                .frame(width: 50, height: 50)
-                .padding(.top, 20)
-
-            // Start Reading button
-            // Start Reading button
-            Button(action: {
-                // When the button is pressed, toggle the showContentView state
-                showContentView.toggle()
-            }) {
-                Text("Start Reading")
-                    .font(.title)
-                    .padding()
-                    .background(Color.blue)
+            VStack {
+                Text("Welcome to LuBooks!")
+                    .font(.largeTitle)
                     .foregroundColor(.white)
-                    .cornerRadius(10)
+                    .padding(.bottom, 20)
+
+                Text("Your Reading Companion")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 40)
+
+                JumpingBookIcon()
+                    .frame(width: 100, height: 100)
+                    .padding(.top, 20)
+
+                // Start Reading button
+                Button(action: {
+                    // When the button is pressed, toggle the showContentView state
+                    showContentView.toggle()
+                }) {
+                    Text("Start Reading")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 50)
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.purple.opacity(0.8)]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(25)
+                }
+                .padding(.top, 20)
             }
         }
     }
 }
+
 
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
