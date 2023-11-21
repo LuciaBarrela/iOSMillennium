f0387ae5f667a30f5a81a366ccad6c6e1d56427c
Lucia Barrela <134049391+LuciaBarrela@users.noreply.github.com>
2023-11-21 11:09:55 +0000

    color background gradient

--- OnboardingFlow/OnboardingFlow/ContentView.swift
+++ OnboardingFlow/OnboardingFlow/ContentView.swift
@@ -7,15 +7,23 @@
 
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            
+            WelcomePage()
+            FeaturesPage()
         }
-        .padding()
+        
+        .background(Gradient(colors: gradientColors))
+        //lets you swipe the .page tag
+        .tabViewStyle(.page)
+        
     }
 }
 
