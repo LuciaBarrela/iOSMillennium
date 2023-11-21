4585c1497f457323a25120266e13218f7877a24b
Lucia Barrela <134049391+LuciaBarrela@users.noreply.github.com>
2023-11-21 16:26:44 +0000

    foreground and background

--- DiceRoller/DiceRoller/ContentView.swift
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -8,22 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var numberOfDice: Int = 1
     var body: some View {
         
+       
+        
         VStack {
+            
+            
             Text("Dice Roller")
                 .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
             
             HStack {
-                ForEach(1...3, id: \.self) { _ in
+                ForEach(1...numberOfDice, id: \.self) { _ in
                     DiceView()
+                        .foregroundColor(.white)
                 }
             }
             
+            HStack {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
+                    numberOfDice -= 1
+                }
+                
+                .disabled(numberOfDice == 1)
+                
+                
+                Button("Add Dice", systemImage: "plus.circle.fill") {
+                    numberOfDice += 1
+                    
+                }
+                
+                .disabled(numberOfDice == 3)
+            }
+            
+            .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
+      
         }
-        
+    
+     
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.colorBack)
+        .tint(.white)
+ 
     }
+       
 }
 
 #Preview {
--- DiceRoller/DiceRoller/DiceView.swift
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -7,33 +7,28 @@
 
 import SwiftUI
 
+
 struct DiceView: View {
-    
-   @State private var numberOfPips: Int = 1
+    @State private var numberOfPips: Int = 1
     
     var body: some View {
-        
         VStack {
-            Image(systemName: "die.face.\(numberOfPips)")
+            Image(systemName: "die.face.\(numberOfPips).fill")
                 .resizable()
-            .frame(width: 120, height: 120)
-       
-        
-        Button("Roll") {
+                .frame(maxWidth: 100, maxHeight: 100)
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundStyle(.black, .white)
             
-            withAnimation {
-                
-                numberOfPips = Int.random(in: 1...6)
-                
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
             }
-            
-            }
-            
-        .buttonStyle(.bordered)
-            
+            .buttonStyle(.bordered)
         }
     }
 }
+
 
 #Preview {
     DiceView()
