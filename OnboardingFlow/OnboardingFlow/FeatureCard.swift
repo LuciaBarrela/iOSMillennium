97c6f88eb8b360f6808651e5995ee079c5954e6e
Lucia Barrela <134049391+LuciaBarrela@users.noreply.github.com>
2023-11-21 10:57:37 +0000

    new start

---
+++ OnboardingFlow/OnboardingFlow/FeatureCard.swift
@@ -0,0 +1,36 @@
+//
+//  FeatureCard.swift
+//  OnboardingFlow
+//
+//  Created by Lucia Barrela on 21/11/2023.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    
+    let iconName: String
+    let description: String
+    
+    var body: some View {
+        
+        HStack {
+            Image(systemName: iconName)
+                .font(.largeTitle)
+                .frame(width: 50)
+                .padding(.trailing, 10)
+            
+            Text(description)
+            
+        }
+        .padding()
+        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .foregroundStyle(.white)
+        
+    }
+}
+
+#Preview {
+    FeatureCard(iconName: "person.2.crop.square.stack.fill",
+                description: "A multiline description about a feature paired with the image on the left.")
+}
